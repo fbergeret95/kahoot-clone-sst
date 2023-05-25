c69d3313278230d4f4f34ef52718b0be53004198
Juanchi <jcesarotti.dev@gmail.com>
2023-05-25 11:19:21 -0300

    new Package adjustments

--- packages/frontend/src/Components/Authenticator_Component.js
+++ packages/frontend/src/Components/Authenticator_Component.js
@@ -1,109 +1,118 @@
-import React from 'react'
-import { css } from 'glamor'
-import { Auth } from 'aws-amplify'
-import SignIn from './SignIn_Component'
-import SignUp from './SignUp_Component'
+import React from "react";
+import { css } from "glamor";
+import { Auth } from "aws-amplify";
+import SignIn from "./SignIn_Component";
+import SignUp from "./SignUp_Component";
 // import ForgotPassword from './ForgotPassword'
 
 class Authenticator_Component extends React.Component {
   state = {
     errorMessage: null,
-    currentState: 'showSignIn'
-  }
-  switchState = currentState => {
+    currentState: "showSignIn",
+  };
+  switchState = (currentState) => {
     this.setState({
-      currentState
-    })
-  }
-  updateErrorMessage = errorMessage => {
-    this.setState({ errorMessage })
-  }
-
+      currentState,
+    });
+  };
+  updateErrorMessage = (errorMessage) => {
+    this.setState({ errorMessage });
+  };
 
   getUserAuthenticated = () => {
     Auth.currentAuthenticatedUser()
-      .then(result => {
+      .then((result) => {
         // this.setState({errorMessage: null, currentState: 'showQuestions'})
-        console.log('User Signed In', result)
-        this.updateErrorMessage('User Signed In')
+        console.log("User Signed In", result);
+        this.updateErrorMessage("User Signed In");
       })
-      .catch(error => {
-        console.log('error getting current session...: ', error)
-      })
-  }
+      .catch((error) => {
+        console.log("error getting current session...: ", error);
+      });
+  };
 
   // getUserAuthenticated()
 
-  componentDidMount(){
-    this.getUserAuthenticated()
+  componentDidMount() {
+    this.getUserAuthenticated();
   }
 
   render() {
-    
-    const { currentState } = this.state
+    const { currentState } = this.state;
 
     return (
       <div style={styles.container}>
-        { currentState === 'showSignIn'  && <SignIn {...this.props} updateErrorMessage={this.updateErrorMessage} />  }
-        { currentState === 'showSignUp' && <SignUp {...this.props} updateErrorMessage={this.updateErrorMessage} switchState={this.switchState} /> }
+        {currentState === "showSignIn" && (
+          <SignIn
+            {...this.props}
+            updateErrorMessage={this.updateErrorMessage}
+          />
+        )}
+        {currentState === "showSignUp" && (
+          <SignUp
+            {...this.props}
+            updateErrorMessage={this.updateErrorMessage}
+            switchState={this.switchState}
+          />
+        )}
         {/* { currentState === 'showForgotPassword' && <ForgotPassword switchState={this.switchState} {...this.props} updateErrorMessage={this.updateErrorMessage} /> } */}
         <div {...css(styles.buttonContainer)}>
-          {
-            currentState === 'showSignIn' ? (
-              <div {...css(styles.linkContainer)}>
-                <p
-                  onClick={() => this.switchState('showSignUp')}
-                  {...css(styles.toggle)}
-                >Need an account? Sign Up</p>
-                {/* <p
+          {currentState === "showSignIn" ? (
+            <div {...css(styles.linkContainer)}>
+              <p
+                onClick={() => this.switchState("showSignUp")}
+                {...css(styles.toggle)}
+              >
+                Need an account? Sign Up
+              </p>
+              {/* <p
                   onClick={() => this.switchState('showForgotPassword')}
                   {...css(styles.toggle)}
                 >Forgot your password?</p> */}
-              </div>
-                ) : (
-              <div {...css(styles.linkContainer)}>
-                <p
-                  {...css(styles.toggle)}
-                  onClick={() => this.switchState('showSignIn')}
-                >Already have an account? Sign In</p>
-              </div>
-            )
-          }
-        </div>
-        {
-          this.state.errorMessage && (
-            <div>
-              <p>{this.state.errorMessage}</p>
             </div>
-          )
-        }
+          ) : (
+            <div {...css(styles.linkContainer)}>
+              <p
+                {...css(styles.toggle)}
+                onClick={() => this.switchState("showSignIn")}
+              >
+                Already have an account? Sign In
+              </p>
+            </div>
+          )}
+        </div>
+        {this.state.errorMessage && (
+          <div>
+            <p>{this.state.errorMessage}</p>
+          </div>
+        )}
       </div>
-    )
+    );
   }
 }
 
-export default Authenticator_Component
+export default Authenticator_Component;
 
 const styles = {
   linkContainer: {
-    marginTop: 30
+    marginTop: 30,
   },
   container: {
-    marginTop: 50
+    marginTop: 50,
   },
   buttonContainer: {
-    display: 'flex',
-    justifyContent: 'center'
+    display: "flex",
+    justifyContent: "center",
   },
   toggle: {
-    paddingBottom: '10px',
-    cursor: 'pointer',
+    paddingBottom: "10px",
+    cursor: "pointer",
     marginTop: 10,
     marginBottom: 0,
     // marginBottom: 0,
-    borderBottom: '2px solid transparent',
-    ':hover': {
-      opacity: 0.6
-    }
-  }
-}
+    borderBottom: "2px solid transparent",
+    ":hover": {
+      opacity: 0.6,
+    },
+  },
+};
