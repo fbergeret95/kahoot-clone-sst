32d8a7010f727c36a2d26c6bc70cfb92ea5caeb5
Juanchi <jcesarotti.dev@gmail.com>
2023-08-31 09:29:08 -0300

    New Adjsutments

--- packages/frontend/src/Components/Leaderboard_Component.js
+++ packages/frontend/src/Components/Leaderboard_Component.js
@@ -33,8 +33,9 @@ function getImageStyleByIndex(index) {
 function Item(data) {
     return (
         <>
-            {data.map((value, index) => (
-                <div className=" p-2 mb-3" key={index}>
+           <div className='row'>
+           {data.map((value, index) => (
+                <div className=" p-2 mb-3 mx-auto my-auto col-lg-4" key={index}>
                     <img
                         className="card-img-top mx-auto"
                         src={getImageByIndex(index)}
@@ -43,13 +44,13 @@ function Item(data) {
                     />
                     <div className="card-body">
                         <strong><h3>{value.username}</h3></strong>
-                       
                         <h5 className="card-title"><strong>{value.time}</strong> Segundos</h5>
                         <p className="card-text">Correctas: <strong>{value.score}</strong></p>
                     </div>
                     <hr className='mx-auto' style={{ width: '50%' }} />
                 </div>
             ))}
+            </div>
         </>
     );
 }
